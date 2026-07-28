328fe1edfbbaf89b12db18a1c0a79751b14a41b8
Vamsy <a.vamc.it@gmail.com>
2026-07-28 12:54:36 +0530

    react

---
+++ 09 React/app02/src/components/Welcome.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const titles = [
+  { title: "Mr.", label: "GentelMan" },
+  { title: "Mrs.", label: "Lady" },
+  { title: "Dr.", label: "Doctor" },
+  { title: "Prof.", label: "Professor" },
+  { title: "Master.", label: "Boy Child" },
+  { title: "Baby.", label: "Girl Child" },
+  { title: "", label: "--SELECT---" }
+];
+
+function Welcome() {
+
+  const [title, setTitle] = useState<string>("");
+  const [userName, setUserName] = useState<string>("SomeBody");
+
+  return (
+    <section>
+      <h3> Welcome {title} {userName}</h3>
+      <form>
+        <label>
+          title:
+          <select
+            value={title}
+            onChange={e => setTitle(e.target.value)}>
+            {
+              titles.map(t => (
+                <option key={t.title} value={t.title}>{t.label}</option>
+              ))
+            }
+          </select>
+        </label>
+        <label>
+          User Name:
+          <input type="text" value={userName}
+            onChange={e => setUserName(e.target.value)} />
+        </label>
+      </form>
+    </section>
+  );
+}
+
+export default Welcome;
